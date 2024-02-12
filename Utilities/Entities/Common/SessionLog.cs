e11c52c03abe5f7db9f629f066a21cea5c1d9fff
onurtekin <onurtekin314@gmail.com>
2024-02-12 18:04:11 +0300

    mini update sessionlog

--- Utilities/Entities/Common/SessionLog.cs
+++ Utilities/Entities/Common/SessionLog.cs
@@ -4,8 +4,8 @@ public abstract class SessionLog : BaseEntity
 {
     public string UserId { get; set; }
     public string Username { get; set; }
-    public string IP { get; set; }
-    public string UserAgent { get; set; }
+    public string IP { get; set; } = string.Empty;
+    public string UserAgent { get; set; } = string.Empty;
     public DateTime LoginTime { get; set; }
     public DateTime? LogoutTime { get; set; }
 }
