d39caea19b7d87aae29167b44d299598518c7059
Onur Tekin <onurtekin314@gmail.com>
2024-02-17 12:56:00 +0300

    INamedEntity eklendi

---
+++ Utilities/Entities/Interfaces/INamedEntity.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Utilities.Entities.Interfaces
+{
+    public interface INamedEntity
+    {
+        [Description("Ad")]
+        string Name { get; set; }
+    }
+}
